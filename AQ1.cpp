fc5189cd086b14d7b3025dd754fcb583a4e8731f
aboudicheng <aboudicheng@gmail.com>
2018-03-29 22:55:28 +0300

    add AQ1.cpp, stack_int.h

--- AQ1.cpp
+++ AQ1.cpp
@@ -1,55 +1,55 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
+#include "stack_int.h"
 
-typedef struct {
-	char name[13];
-	int stock, arrive;
-} phones_t;
+int postfix(char *string) {
+	stack_t s;
+	initialize_s(&s);
 
-int rbinary_search(FILE *ptr, char *brand, int top, int bottom, int *step) {
-	int middle;
-	if (top <= bottom) {
-		(*step)++;
-		middle = (top + bottom) / 2;
-		phones_t temp;
-		fseek(ptr, (middle - 1) * sizeof(phones_t), SEEK_SET);
-		fread(&temp, sizeof(phones_t), 1, ptr);
+	int i = 0, len = strlen(string), num, res, a, b;
+	while (i < len) {
+		if (string[i] >= '0' && string[i] <= '9') {
+			num = string[i] - '0';
+			push(&s, num);
+		}
+		else if (string[i] == '+') {
+			b = pop(&s);
+			a = pop(&s);
+			res = a + b;
+			push(&s, res);
+		}
 
-		if (strcmp(temp.name, brand) == 0)
-			return middle;
-		else if (strcmp(temp.name, brand) > 0)
-			return rbinary_search(ptr, brand, top, middle - 1, step);
-		else
-			return rbinary_search(ptr, brand, middle + 1, bottom, step);
+		else if (string[i] == '-') {
+			b = pop(&s);
+			a = pop(&s);
+			res = a - b;
+			push(&s, res);
+		}
+
+		else if (string[i] == '*') {
+			b = pop(&s);
+			a = pop(&s);
+			res = a * b;
+			push(&s, res);
+		}
+
+		else if (string[i] == '/') {
+			b = pop(&s);
+			a = pop(&s);
+			res = a / b;
+			push(&s, res);
+		}
+		i++;
 	}
-	else
-		return -1;
+
+	return res;
 }
 
 int main(void) {
-	FILE *ptr = fopen("phones.bin", "rb");
-	if (ptr == NULL)
-		printf("Cannot open file.\n");
-	else {
-		phones_t temp;
-		fseek(ptr, 0, SEEK_END);
-		int size = ftell(ptr) / sizeof(phones_t);
-		rewind(ptr);
+	char exp[20];
+	printf("Enter an expression: ");
+	scanf("%[^\n]", exp);
+	printf("The result is %d\n", postfix(exp));
 
-		int step = 0;
-		char brand[13];
-		printf("Enter a phone brand to search: ");
-		scanf("%s", brand);
-
-		int res = rbinary_search(ptr, brand, 0, size, &step);
-		if (res != -1) {
-			fseek(ptr, (res - 1) * sizeof(phones_t), SEEK_SET);
-			fread(&temp, sizeof(phones_t), 1, ptr);
-
-			printf("After %d step(s) %s found with %d stocks and %d stocks will arrive\n", step, temp.name, temp.stock, temp.arrive);
-		}
-		else
-			printf("After %d step(s) %s could not be found!\n", step, brand);
-	}
+	return 0;
 }
